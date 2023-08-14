8452e4957e34d0dc90de20e4dcb5171d2ca829b3
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-08-14 13:26:41 +0300

    Add project and question two

---
+++ app/src/main/java/com/example/sisterslabakademikotlinodevlerihafta5/soru1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sisterslabakademikotlinodevlerihafta5.soru1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sisterslabakademikotlinodevlerihafta5.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
